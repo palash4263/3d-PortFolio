64b99e254634f9bb30918bdcfbb66a070942817e
Palash123-create <palash.mishra@nexgeniots.com>
2026-05-05 23:09:16 +0530

    Fixed lowerCase import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { motion } from "framer-motion";
 
 import Loader from "../components/Loader";
-import { Island } from "../models/island";
+import { Island } from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
